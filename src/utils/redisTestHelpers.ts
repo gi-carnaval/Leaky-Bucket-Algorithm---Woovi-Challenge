2c6b454dfca1e32695d2e8fa9dc6dc54d5537d8f
Giovani Carnaval <gi.carnaval@hotmail.com>
2025-08-19 00:40:10 -0300

    fix: remove create user bucket from src/utils

--- src/utils/redisTestHelpers.ts
+++
@@ -1,8 +0,0 @@
-import { client } from "../lib/redis";
-
-export async function createOrUpdateUserBucket(userToken: string, tokens: number) {
-  await client.hSet(userToken, {
-    tokens_count: tokens.toString(),
-    last_request: Date.now().toString()
-  })
-}
